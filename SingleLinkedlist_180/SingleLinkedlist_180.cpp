8e12b0659a537053343c75355e391731d8efe1bf
dzulfaqaar <fikri6aul@gmail.com>
2023-05-19 11:08:40 +0700

    if

--- SingleLinkedlist_180/SingleLinkedlist_180.cpp
+++ SingleLinkedlist_180/SingleLinkedlist_180.cpp
@@ -20,4 +20,12 @@ void addnode() {
 	nodebaru->noMhs = nim;
 	nodebaru->name = nama;
 
-	
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "Nim sudah ada" << endl;
+			return;
+		}
+		node* current = START;
+		node* previous = START;
+
+		
